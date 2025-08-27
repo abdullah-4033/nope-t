7222815ec530631678c446916dbadb5227e1b6d2
abdullah <abdullah.king@gmail.com>
2025-08-27 05:10:28 -0700

    abdullah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 const page = () => {
   return (
     <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/mian.jpeg" alt="" width="384" height="512"/>
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/abdullah.jpg" alt="" width="384" height="512"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
-        “mian brand.”
+        “abdullah brand.”
       </p>
     </blockquote>
     <figcaption className="font-medium">
       <div className="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
+        abdullah shahid
       </div>
       <div className="text-slate-700 dark:text-slate-500">
         Staff Engineer, Algolia
@@ -20,7 +20,7 @@ const page = () => {
     </figcaption>
   </div>
 </figure><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/mian.jpeg" alt="" width="384" height="512"/>
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/abdullah.jpg" alt="" width="384" height="512"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
@@ -29,7 +29,7 @@ const page = () => {
     </blockquote>
     <figcaption className="font-medium">
       <div className="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
+        asking
       </div>
       <div className="text-slate-700 dark:text-slate-500">
         Staff Engineer, Algolia
@@ -37,16 +37,16 @@ const page = () => {
     </figcaption>
   </div>
 </figure><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/mian.jpeg" alt="" width="384" height="512"/>
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/abdullah.jpg" alt="" width="384" height="512"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
-        “mian fan club.”
+        “tailwand is the most important thing.”
       </p>
     </blockquote>
     <figcaption className="font-medium">
       <div className="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
+        abdullah bro
       </div>
       <div className="text-slate-700 dark:text-slate-500">
         Staff Engineer, Algolia
